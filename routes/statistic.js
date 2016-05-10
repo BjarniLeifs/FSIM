4f5d9eda85402e39add03ddc4aa84f6795c0977d
BjarniLeifs <bjarnil10@ru.is>
2016-05-10 23:15:44 +0000

    indexloan ready most likley

--- routes/statistic.js
+++ routes/statistic.js
@@ -10,28 +10,28 @@ router.get('/einn', function(req, res, next) {
 
 
 router.get('/indexloan', function(req, res, next) {
-	var capital;
-	var payInterest;
-	var Principal = 20000000;//req.body.principal,
-	var Interest = 0.04;//req.body.interest,
-	var duration = 300;//req.body.duration,
-
+	/* Vars that needs to have sent.*/
+	var Principal 	= 20000000;//req.body.principal,
+	var Interest 	= 0.055;//req.body.interest,
+	var duration 	= 480;//req.body.duration,
+	var CPI 		= 0.1;//req.body.cpi;
 	var D = ((30.0)/(360.0));
-	var CPI = 0.1;//req.body.cpi;
 	var inflation = Math.pow((1.0 + CPI ),(1.0/12.0)) - 1;
-	var AF = [];
-	var P = [];
-	
-	//var interest = [];
-	var II = [];
-	
 
-	var Increase = 0;
-	var total = 0;
+	/* Array to return later, keep track of things */
+	var AF 			= [];
+	var P 			= [];
+	var payment 	= [];
+	var capital 	= [];
+	var payInterest = [];
+	var II 			= [];
+	var Increase 	= [];
+	/* var control */
+	
+	var total 		= 0;
+	/* iterator initialation*/
 	var it;
-	var payment;
-	var test = Math.pow(2,3);
-	console.log(test);
+
 
 	for (it = 0; it < duration; it++) {
 
@@ -40,30 +40,41 @@ router.get('/indexloan', function(req, res, next) {
 		if (it == 0) {
 			II.push(100 + 100 * inflation);
 			P.push(Principal * II[0]/100);
-			Increase = P[0] - Principal;
+			Increase.push(P[0] - Principal);
 			
 		} else {
 
 			II.push(II[it-1] + II[it-1]*inflation);
-			P.push((P[it-1] - capital) * II[it]/II[it-1]);
+			P.push((P[it-1] - capital[it-1]) * II[it]/II[it-1]);
 
-			Increase = ((P[it-1] - capital) * II[it]/II[it-1]) - (P[it-1] - capital);
+			Increase.push((P[it-1] - capital[it-1]) * II[it]/II[it-1]) - (P[it-1] - capital[it-1]);
 
 		}
 
 
-		payment  = P[it]/AF[it];
+		payment.push(P[it]/AF[it]);
 
-		payInterest = P[it] * Interest * D;
-		console.log('pay : '+ payment+ ' Int : ' + payInterest);
-		capital  = payment - payInterest;
-		total += capital;
+		payInterest.push(P[it] * Interest * D);
+		
+		capital.push(payment[it] - payInterest[it]);
+		
+		total += capital[it];
+		
 		
 	}
+	var objReturn = {
+  		AF 			: AF,
+		P 			: P,			
+		payment 	: payment,
+		capital 	: capital,
+		payInterest : payInterest,
+		II 			: II,
+		Increase 	: Increase,
+		total 		: total
+	};
 
-console.log(total);
 
-  	return res.status(200).json({messgae : 'yes'});
+  	return res.status(200).json(objReturn);
 });
 
 
