7b09e0bbee58abb050f9bfe94ecdb959b59f1039
SergioMyth <isleifurmj@gmail.com>
2016-05-11 14:39:25 +0000

    Changes made to index and controller of the compoundloan.

--- public/assets/mainbuild/myApp.js
+++ public/assets/mainbuild/myApp.js
@@ -41,10 +41,28 @@ app.config([ '$stateProvider', '$urlRouterProvider', '$locationProvider', '$http
         $translateProvider.useSanitizeValueStrategy('escape');
     }
 ]);
-app.controller('CompoundCtrl', ['$scope', '$state', '$timeout',
-    function ($scope, $state, $timeout) {
+app.controller('CompoundCtrl', ['$scope', '$state', '$timeout', 'IndexedFact', 
+    function ($scope, $state, $timeout, IndexedFact) {
 
+IndexedFact.getIndexedloan().then(function(response){
 
+    		$scope.indexedLoan =  response.data;
+
+		});
+		$scope.test;
+		$scope.youPercent;
+		$scope.empPercent;
+		$scope.mSalary;
+		$scope.indexedFloat;
+		$scope.indexedFixed;
+		$scope.nonIndexedFloat;
+		$scope.nonIndexedFixed;
+		$scope.aPayment
+		$scope.ePayment
+		$scope.loanAmount;
+		$scope.propertyValue;
+		$scope.intresetRate;
+		$scope.inflation;
     }
 ]);
 app.controller('ContributorsCtrl', ['$scope', '$state', '$timeout',
